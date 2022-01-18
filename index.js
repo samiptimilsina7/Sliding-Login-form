fba3edd0e160d5478856a5630ddddf3d5a836791
Samip Timilsina <timi.samip@gmail.com>
2022-01-19 00:05:05 +0545

    adds slide for signin

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 boxEl=document.querySelector(".form-box");
+body=document.querySelector("body");
+signUp
 
 function signup(){
     boxEl.classList.add('transition');
+    body.classList.add('color-change');
     console.log('sigup')
 }
 
 function signin(){
     boxEl.classList.remove('transition');
+    body.classList.remove('color-change');
     console.log('sigin')
 }
