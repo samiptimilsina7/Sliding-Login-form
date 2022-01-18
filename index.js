5d02a7ea0b80281aa2f01b99b1cbc19d67a60be8
Samip Timilsina <timi.samip@gmail.com>
2022-01-18 19:34:15 +0545

    adds translate property

---
+++ index.js
@@ -0,0 +1,11 @@
+boxEl=document.querySelector(".form-box");
+
+function signin(){
+    boxEl.classList.add('transition-to-left');
+    console.log('sigin')
+}
+
+function signup(){
+    boxEl.classList.add('transition-to-right');
+    console.log('signup')
+}
